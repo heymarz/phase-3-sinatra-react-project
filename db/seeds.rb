5e333934d2a8e44f30758de8338ea0dbcdc9fc2b
Marlena Tsang <Marlena345@gmail.com>
2022-02-15 11:22:25 -0500

    data input

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,30 @@
 puts "🌱 Seeding spices..."
 
-Location.create(name: "Pennsylvania" attraction: "Mutter Museum" dining: nil)
-Location.create(name: "Chicago" attraction: "Grant Park and Buckingham Fountain", "Adler Planetarium", "Shedd Aquarium", "Chicago Cultural Center" dining: "Superdawg Drive-In", "Lou Malnati's")
-Location.create(name: "New York" attraction: "Lake George", "Angry Orchard Vineyard" dining: "SakeMai")
-Location.create(name: "Colorado" attraction: "Rocky Mountain National Park", "Mesa Verde National Park" dining: "Far View Lounge")
-Location.create(name: "Wyoming" attraction: "Grand Prismatic Spring" dining: nil)
-Location.create(name: "California" attraction: "Yosemite National Park", "Vichy Springs" dining: nil)
-Location.create(name: "Arizona" attraction: "Grand Canyon National Park" dining: "El Tovar Dining Room")
-Location.create(name: "Oregon" attraction: "Portland Japanese Garden", "Pittock Mansion", "Crystal Springs Rhododendron Garden" dining: "Bhuna", "Casa Zoraya")
+Location.create(state: "Pennsylvania") 
+Location.create(state: "Chicago") 
+Location.create(state: "New York") 
+Location.create(state: "Colorado") 
+Location.create(state: "Wyoming") 
+Location.create(state: "California") 
+Location.create(state: "Arizona") 
+Location.create(state: "Oregon") 
+
+Attraction.create(attraction: "Mutter Museum")
+Attraction.create(attraction: "Grant Park and Buckingham Fountain", "Adler Planetarium", "Shedd Aquarium", "Chicago Cultural Center")
+Attraction.create(attraction: "Lake George", "Angry Orchard Vineyard")
+Attraction.create(attraction: "Rocky Mountain National Park", "Mesa Verde National Park")
+Attraction.create(attraction: "Grand Prismatic Spring")
+Attraction.create(attraction: "Yosemite National Park", "Vichy Springs")
+Attraction.create(attraction: "Grand Canyon National Park")
+Attraction.create(attraction: "Portland Japanese Garden", "Pittock Mansion", "Crystal Springs Rhododendron Garden")
+
+Dining.create(place_to_eat: nil)
+Dining.create(place_to_eat: "Superdawg Drive-In", "Lou Malnati's")
+Dining.create(place_to_eat: "SakeMai")
+Dining.create(place_to_eat: "Far View Lounge")
+Dining.create(place_to_eat: nil)
+Dining.create(place_to_eat: nil)
+Dining.create(place_to_eat: "El Tovar Dining Room")
+Dining.create(place_to_eat: "Bhuna", "Casa Zoraya")
 
 puts "✅ Done seeding!"
