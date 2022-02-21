12a7c918baf0440138bb4c86c9c3f8eac01fbfc1
Marlena Tsang <Marlena345@gmail.com>
2022-02-20 22:24:12 -0500

    added useEffect and useState in parent component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
-import Navbar from "./components/navigation/navigation/Navbar";
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import Navbar from "./components/navigation/Navbar";
+import Home from './components/Home'
+import AddNew from './components/AddNew';
+import LocationCard from './components/LocationCard'
+import ErrorPage from './components/ErrorPage';
  
 function App() {
+  const [states, setStates] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/locations')
+    .then(resp => resp.json())
+    .then (data => setStates(data))
+  },[])
+
+
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+        <Routes>
+          <Route path="/" element= {<Home 
+            states={states}/>} 
+          />
+          <Route path=":id" element= {<LocationCard      
+            states={states} />} 
+          />
+          <Route path="/locations/new" element={<AddNew 
+            setStates={setStates} />} 
+          />
+          <Route  path="*" element={ <ErrorPage /> } />
+        </Routes>
+    </Router>
   );
 }
 
