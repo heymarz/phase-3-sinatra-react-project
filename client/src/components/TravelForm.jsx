ddbb49c862897cb165eeefdb46a379ac115d5913
Marlena Tsang <Marlena345@gmail.com>
2022-02-22 14:53:06 -0500

    setting up form

--- client/src/components/TravelForm.jsx
+++ client/src/components/TravelForm.jsx
@@ -4,7 +4,29 @@ function TravelForm({ setLocations }) {
   return (
     <div>
       <h1>Add New Location to Travel Wish List</h1>
-    
+      <section>
+      <form>
+        <label htmlFor="state">Name:
+          <input type="text"
+            placeholder='City, State'
+          />
+        </label>
+        <br />
+        <label htmlFor="attractions">Attraction(s):
+          <input type="text"
+            placeholder='place 1, place 2 ...'
+          />
+        </label>
+        <br />
+        <label htmlFor="restaurants">Restaurant(s):
+          <input type="text"
+            placeholder='Restaurant 1, Restaurant 2 ...'
+          />
+        </label>
+        <br />
+        <button>Submit</button>
+      </form>
+      </section>
     </div>
   )
 }
