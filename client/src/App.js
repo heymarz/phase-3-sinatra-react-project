1a1086deeb5d22ef80cc6fdad7295f7bc35df508
Marlena Tsang <Marlena345@gmail.com>
2022-02-22 13:36:48 -0500

    added useEffect and useState hooks

--- client/src/App.js
+++ client/src/App.js
@@ -2,32 +2,34 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import Navbar from "./components/navigation/Navbar";
 import Home from './components/Home'
-import AddNew from './components/AddNew';
-import LocationCard from './components/LocationCard'
+import TravelForm from './components/TravelForm';
+import Search from './components/Search'
 import ErrorPage from './components/ErrorPage';
  
 function App() {
-  const [states, setStates] = useState([])
+  const [locations, setLocations] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch('http://localhost:9292/locations')
     .then(resp => resp.json())
-    .then (data => setStates(data))
+    .then (data => setLocations(data))
   },[])
 
+    function handleSearch(newSearch) {
+      setSearch(newSearch)
+    }
 
   return (
     <Router>
       <Navbar />
         <Routes>
-          <Route path="/" element= {<Home 
-            states={states}/>} 
+          <Route path="/" element= {<Home locations={locations} />} 
           />
-          <Route path=":id" element= {<LocationCard      
-            states={states} />} 
+          <Route path=":id" element= {<Search handleSearch={handleSearch} search={search}/>} 
           />
-          <Route path="/locations/new" element={<AddNew 
-            setStates={setStates} />} 
+          <Route path="/locations/new" element={<TravelForm 
+            setLocations={setLocations} />} 
           />
           <Route  path="*" element={ <ErrorPage /> } />
         </Routes>
