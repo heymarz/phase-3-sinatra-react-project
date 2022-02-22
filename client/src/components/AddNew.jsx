768edfc6b2ad8cf7f1a28e98eebd8e6be23e6ede
Marlena Tsang <Marlena345@gmail.com>
2022-02-22 13:35:17 -0500

    Renamed component

--- client/src/components/AddNew.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AddNew() {
-  return (
-    <div>Add New Location to Travel Wish List</div>
-  )
-}
-
-export default AddNew;
---
+++ client/src/components/TravelForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TravelForm({ setLocations }) {
+  return (
+    <div>
+      <h1>Add New Location to Travel Wish List</h1>
+    
+    </div>
+  )
+}
+
+export default TravelForm;
