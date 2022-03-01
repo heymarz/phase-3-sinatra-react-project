fdec804890c056cb1ac68fcecfa5c9ce9acc03ea
Marlena Tsang <Marlena345@gmail.com>
2022-03-01 13:34:52 -0500

    moved useEffect and searchbar working

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,50 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import Navbar from "./components/navigation/Navbar";
 import Home from './components/Home'
 import TravelForm from './components/TravelForm';
 import ErrorPage from './components/ErrorPage';
+import LocationCard from './components/LocationCard';
+import { baseUrl } from './Global';
  
 function App() {
   const [locations, setLocations] = useState([]);
   const [search, setSearch] = useState("")
 
-  function handleAddTravelPlace(newPlace) {
-    setLocations([...locations, newPlace]);
+  useEffect(() => {
+    fetch(baseUrl + '/locations')
+  .then(resp => resp.json())
+  .then (data => setLocations(data))
   }
+  ,[])
   
+  function handleDelete(id){
+    const newLocationsArray = locations.filter((location)=>location.id !== id);
+    setLocations(newLocationsArray)
+  }
+
+  const filterLocations = locations.filter((loc)=>{
+    return loc.state.toLowerCase().includes(search.toLowerCase())
+  })
+
+  const renderLocations = filterLocations.map((location)=>{
+    return <LocationCard 
+      key={location.id}
+      location={location}
+      handleDelete={handleDelete}
+    />
+  })
+
   return (
     <Router>
       <Navbar />
         <Routes>
           <Route path="/" element= {<Home 
-          locations= {locations}
-          setLocations={setLocations}
           search={search}
           setSearch={setSearch}
+          renderLocations={renderLocations}
           />} />
-          <Route path="/locations/new" element={<TravelForm onNewTravelPlace ={handleAddTravelPlace}
-          />} />
+          <Route path="/locations/new" element={<TravelForm />} />
           <Route  path="*" element={ <ErrorPage /> } />
         </Routes>
     </Router>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,44 +1,7 @@
-import React,{ useEffect, useRef } from 'react'
-import LocationCard from './LocationCard'
+import React from 'react';
 import Search from './Search';
-import { baseUrl } from '../Global'
 
-function Home({ locations, setLocations, search, setSearch }) {
-  const tempFunction = useRef();
-
-  const fetchFunction = () =>{
-  fetch(baseUrl + '/locations')
-  .then(resp => resp.json())
-  .then (data => setLocations(data))
-  }
-  tempFunction.current = fetchFunction 
-
-  useEffect(() => {
-    tempFunction.current()
-  },[])
-
-  function handleDelete(id){
-    const newLocationsArray = locations.filter((location)=>location.id !== id);
-    setLocations(newLocationsArray)
-  }
-  
-  function locationsToRender(){
-
-  const displayWishList = locations.map((locations)=> {
-    return <LocationCard 
-    key={locations.id}
-    locations = {locations}
-    handleDelete={handleDelete}
-    />})
-    
-        if(!search){
-          return displayWishList
-        } else {
-          return displayWishList.filter((location)=>{
-            return location.state.toLowerCase().includes(search.toLowerCase())
-        }) 
-      }
-    }
+function Home({ renderLocations, search, setSearch}) {
 
   return (
     <div>
@@ -46,10 +9,11 @@ function Home({ locations, setLocations, search, setSearch }) {
         My Travel Wish List
       </h1>
       <Search 
-        onSearch = {setSearch}
+        search={search}
+        setSearch={setSearch}
       />
       <div>
-       {locationsToRender()}
+        {renderLocations}
       </div>
     </div>
   )
